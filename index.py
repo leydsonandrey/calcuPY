b4da8bdbe966abd1e9d573d9f14da2532ef627ce
SureAndrey <sureandrey@gmail.com>
2023-01-10 10:44:20 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,34 @@
+calculos_feitos = 0
+
+
+def quit(feitos):
+    print(f'foram feitos {feitos} calculos')
+    print("saindo...")
+
+
+def help():
+    print('digite "quit" para sair\ndigite "help" para ajuda')
+
+
+def calcular(calculo):
+    try:
+        print(f'| {eval(calculo)}')
+        print('|')
+    except Exception as e:
+        return print(f'| Calculo escrito errado :  {str(e)}')
+
+
+print("| CALCULADORA")
+
+while True:
+    calculo = input('| : ')
+    if "quit" in calculo:
+        quit(calculos_feitos)
+        break
+    elif "help" in calculo:
+        help()
+    elif calculo.isalpha() or not "quit" or not "help":
+        print("| Letras não são permitidas")
+    else:
+        calcular(calculo)
+        calculos_feitos += 1
