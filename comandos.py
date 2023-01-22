9e515f37f6fff2ad2c547b3e1e0c51c5d46d73d0
Andrey Duarte <sureandrey@gmail.com>
2023-01-22 10:35:10 -0300

    fix(comandos): condiçôes para mensagem de calculos feitos

--- comandos.py
+++ comandos.py
@@ -11,7 +11,12 @@ def contas_feitas():
 
 def mostrar_contas_feitas():
     global calculos_feitos
-    print(f"\nForam feitos: {calculos_feitos} calculos\n")
+    if calculos_feitos == 0:
+        print(f"\nNem um calculo feito\n")
+    elif calculos_feitos == 1:
+        print(f"\nFoi feito: {calculos_feitos} calculo\n")
+    elif calculos_feitos > 1:
+        print(f"\nForam feitos: {calculos_feitos} calculos\n")
 
 
 def sair():
@@ -26,7 +31,3 @@ def ajuda():
         'digite "ajuda" para ajuda \n'
         'digite "contas" para ver a quantidade de contas feitas \n'
     )
-
-
-
-
